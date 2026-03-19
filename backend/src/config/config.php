d07f11a5524c30c43de50a06a7b9aa1a5b8fbae0
Ermindo Treviso Neto <126285198+protileon-ermes@users.noreply.github.com>
2026-03-19 11:08:11 -0300

    Add files via upload

---
+++ backend/src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$dataFile = __DIR__ . '/../../data/data.json';
+
+$allowedOrigins = [
+    'http://localhost:8081', 
+    'http://127.0.0.1:8081', 
+    'http://172.18.0.1'
+];
---
+++ backend/src/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../config/config.php';
+
+//cors
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+in_array($origin, $allowedOrigins) ?
+    header("Access-Control-Allow-Origin: $origin") : null;
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+//roteamento
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+
+match ($uri) {
+    '/api/users' => require_once __DIR__ . '/../src/api.php',
+    '/swagger' => require_once __DIR__ . '/swagger-ui.html',
+    default => notFound()
+};
+
+function notFound()
+{
+    http_response_code(404);
+    echo json_encode(['error' => 'Endpoint not found']);
+    exit;
+}
